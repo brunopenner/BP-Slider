fe58628036b0dd24a564e6af2ccf630e6f232c1b
Bruno Penner <brunopenner01@gmail.com>
2025-10-02 10:14:03 +0800

    Plugin Creation

---
+++ mv-slider.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Plugin Name: BP Slider
+ * Plugin URI: https://brunopenner.com.au/
+ * Description: Slideshow Wordpress plugin based on slick slider
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Bruno Penner
+ * Author URI: https://brunopenner.com.au/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: bp-slider
+ * Domain Path: /languages
+ */
