bf8234efa8f427e0a87d3dcfd1a81544d1a5e52a
Bruno Penner <brunopenner01@gmail.com>
2025-10-02 10:18:54 +0800

    die to prevent users from accessing the plugin files directly

--- mv-slider.php
+++ mv-slider.php
@@ -13,3 +13,23 @@
  * Text Domain: bp-slider
  * Domain Path: /languages
  */
+
+/*
+BP Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+BP Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BP Slider. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'ABSPATH') ) {
+    die('Get out of here.')
+    exit;
+}
